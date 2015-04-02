c1dc9267f84466c743f5d563509ad273778886e0
Daniel Dittenhafer <daniel@dittenhafer.net>
2015-04-01 23:48:00 -0400

    int64 adjustment

--- pangviz.py
+++ pangviz.py
@@ -12,6 +12,7 @@ def ToGvizDataTable(dataframe):
     table = gv.Table()
     dt = dataframe.dtypes
 
+    # Loop to add the columns from the dataframe to the gviz datatable
     for col in dataframe.columns.values:
         gvdt = dt[col]
         if dt[col] == object:
@@ -21,7 +22,10 @@ def ToGvizDataTable(dataframe):
             gvdt = float
         elif dt[col] == "datetime64[ns]":
             gvdt = datetime
+        elif dt[col] == "int64":
+            gvdt = int
 
+        # If a datatype was specified, then add the column
         if gvdt != None:
             table.add_column(col, gvdt)
 
@@ -32,6 +36,8 @@ def ToGvizDataTable(dataframe):
             nv = v
             if (type(v) is float) and numpy.isnan(v):
                nv = None
+            elif type(v) is long:
+                nv = int(v)
             elif type(v) is pd.tslib.Timestamp:
                 nv = datetime(v.year, v.month, v.day)
             newVals.append(nv)
@@ -41,7 +47,12 @@ def ToGvizDataTable(dataframe):
     return table
 
 def parseDateYearMonth(year, month):
-
+    """
+    Helper method for converting individual year and month columns into a first of the month date.
+    :param year:
+    :param month:
+    :return: the first of the month, year datetime object
+    """
     if type(year) is str:
         year = int(year)
 
@@ -56,11 +67,13 @@ def parseDateYearMonth(year, month):
 
 def main():
     """Our cheap unit test main function."""
-    data = pd.read_table("C:\Code\R\IS608-VizAnalytics\FinalProject\Data\Natality, 2007-2013-StateCounty.txt",
+    #dataFile = "C:\Code\R\IS608-VizAnalytics\FinalProject\Data\Natality, 2007-2013-StateCounty.txt"
+    dataFile = "C:\Code\R\IS608-VizAnalytics\FinalProject\Data\LA-Natality-Combined.csv"
+    data = pd.read_table(dataFile, sep=",",
                          parse_dates={'Date': ["Year Code", "Month Code"]}, date_parser=parseDateYearMonth)
     data["Date"] = pd.to_datetime(data["Date"])
     #data["Date"] = "{0}/{1}/01".format(data["Year"], data["Month"])
-    dataStateSum = data.groupby(["State", "Date"])["Births"].sum().reset_index()
+    dataStateSum = data.groupby(["State", "Date", "UnemploymentRate"])["Births"].sum().reset_index()
     #dataStates
     print(dataStateSum.head())
 
